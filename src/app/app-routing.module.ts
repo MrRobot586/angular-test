137c8fd941af1c0ab669cc557261e8e2f2d09559
MrRobot_586 <diegojoseoropeza@gmail.com>
2022-08-06 18:03:41 -0500

    Navbar: Routes configuration and pages added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Components imports
+import { HomeComponent } from './pages/components/home/home.component';
+import { AllCharactersComponent } from './pages/components/all-characters/all-characters.component';
+import { StudentsComponent } from './pages/components/students/students.component';
+import { TeachersComponent } from './pages/components/teachers/teachers.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'allcharacters', component: AllCharactersComponent },
+  { path: 'students', component: StudentsComponent },
+  { path: 'teachers', component: TeachersComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Components imports
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { AllCharactersComponent } from './pages/components/all-characters/all-characters.component';
+import { TeachersComponent } from './pages/components/teachers/teachers.component';
+import { StudentsComponent } from './pages/components/students/students.component';
+import { HomeComponent } from './pages/components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AllCharactersComponent,
+    TeachersComponent,
+    StudentsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
