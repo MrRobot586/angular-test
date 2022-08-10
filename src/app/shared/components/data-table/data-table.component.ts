1179365ea4d3853e0d2e9e97ef93dda85ff5f206
MrRobot_586 <diegojoseoropeza@gmail.com>
2022-08-10 04:25:31 -0500

    API: Update, some changes to the data-table component to load information faster

--- src/app/shared/components/data-table/data-table.component.ts
+++ src/app/shared/components/data-table/data-table.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router, Params } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { HpApiService } from '../../services/hp-api.service';
 import { CharacterData } from '../../models/character-data';
@@ -12,18 +13,23 @@ import { CharacterData } from '../../models/character-data';
 })
 export class DataTableComponent implements OnInit {
 
+  public URLParamsSub!: Subscription;
 
   public DataFromApi:CharacterData[] = []; 
 
   constructor(private ApiService:HpApiService, private route:ActivatedRoute, private router:Router) { }
 
   ngOnInit(): void { 
-    this.route.params.subscribe((params: Params) => {
+    this.URLParamsSub = this.route.params.subscribe(() => {
       let query = '';
       query = this.ApiService.ParseQuery(this.router.url.split('/'));
       this.ApiService.GetDataFromApi(query).subscribe(data=>{
         this.DataFromApi = data;
       });
     });
+  }
+
+  ngOnDestroy():void{
+    this.URLParamsSub?.unsubscribe();
   }
 }
